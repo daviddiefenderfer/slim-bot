3916848fb6b5aff94986c1690e840e8c569a56d9
Jarek O <jimmyd@Jareks-MacPro.local>
2016-12-04 21:53:47 -0500

    Update demo page

--- demo/main.ts
+++ demo/main.ts
@@ -10,16 +10,53 @@ const myCommands = {
         }
     },
     'backgroundColor': {
-        params: ['hex code'],
+        params: ['Enter a hex code'],
         operation: function(color) {
             document.body.style.backgroundColor = color;
         }
+    },
+    'big': {
+        params: [],
+        operation: function() {
+          input.blur();
+          input.classList.add('big');
+          setTimeout(function() {
+            input.classList.remove('big');
+            input.focus();
+          }, 1000);
+        }
+    },
+    'light': {
+        params: [],
+        operation: function() {
+            document.body.style.backgroundColor = 'white';
+            input.style.color = '#222';
+            var commandList = document.getElementById('command-list');
+            commandList.style.color = 'initial';
+        }
+    },
+    'dark': {
+        params: [],
+        operation: function() {
+            document.body.style.backgroundColor = '#333';
+            input.style.color = 'white';
+            var commandList = document.getElementById('command-list');
+            commandList.style.color = 'white';
+        }
     }
 };
+
+const keys = Object.keys(myCommands)
+const list = document.getElementById('availableCommands');
+
+keys.forEach(function(key){
+    const li = document.createElement('li');
+    li.innerText = '/' + key;
+    list.appendChild(li);
+})
 
 const bot = new Tokenize ({
     element: input,
     commands: myCommands,
     prefix: '/'
 });
-
